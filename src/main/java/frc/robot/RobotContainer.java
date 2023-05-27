214764e5713d3ee4ad429f9c017a244be0cc5a70
Weaver Goldman <we.goldm@gmail.com>
2023-05-27 10:46:27 -0400

    Here is the fully functional code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,40 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Drive;
 
 public class RobotContainer {
+	Joystick leftJoystick = new Joystick(0);
+	Joystick rightJoystick = new Joystick(1);
+
+    private Drive drive;
+
     public RobotContainer() {
+        drive = new Drive();
+
         configureBindings();
+	}
+
+    private void configureBindings() {
+        drive.setDefaultCommand(
+                drive.driveCommand(this::getLeftAxis, this::getRightAxis));
     }
 
-    private void configureBindings() {}
+	private double getLeftAxis() {
+		return -deadband(leftJoystick.getY(), 0.05);
+	}
+
+	private double getRightAxis() {
+		return -deadband(rightJoystick.getX(), 0.05);
+	}
+
+	private static double deadband(double value, double tolerance) {
+        if (Math.abs(value) < tolerance) return 0.0;
+
+        return Math.copySign(value, (value - tolerance) / (1.0 - tolerance));
+    }
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,76 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+
+public class Drive extends SubsystemBase {
+    private CANSparkMax leftLeaderMotor;
+    private CANSparkMax leftFollowerMotor;
+    private CANSparkMax rightLeaderMotor;
+    private CANSparkMax rightFollowerMotor;
+
+    public Drive() {
+        leftLeaderMotor = new CANSparkMax(4, MotorType.kBrushless);
+        leftFollowerMotor = new CANSparkMax(3, MotorType.kBrushless);
+        rightLeaderMotor = new CANSparkMax(1, MotorType.kBrushless);
+        rightFollowerMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+        leftLeaderMotor.restoreFactoryDefaults();
+        rightLeaderMotor.restoreFactoryDefaults();
+        leftFollowerMotor.restoreFactoryDefaults();
+        rightFollowerMotor.restoreFactoryDefaults();
+
+        leftLeaderMotor.setInverted(false);
+        rightLeaderMotor.setInverted(true);
+        leftFollowerMotor.follow(leftLeaderMotor, false);
+        rightFollowerMotor.follow(rightLeaderMotor, false);
+
+        leftLeaderMotor.enableVoltageCompensation(12.0);
+        rightLeaderMotor.enableVoltageCompensation(12.0);
+        leftLeaderMotor.setSmartCurrentLimit(30);
+        rightLeaderMotor.setSmartCurrentLimit(30);
+
+        leftFollowerMotor.setIdleMode(IdleMode.kBrake);
+        rightFollowerMotor.setIdleMode(IdleMode.kBrake);
+        leftLeaderMotor.setIdleMode(IdleMode.kBrake);
+        rightLeaderMotor.setIdleMode(IdleMode.kBrake);
+
+        leftLeaderMotor.burnFlash();
+        rightLeaderMotor.burnFlash();
+        leftFollowerMotor.burnFlash();
+        rightFollowerMotor.burnFlash();
+    }
+
+    public Command driveCommand(DoubleSupplier leftAxis, DoubleSupplier rightAxis) {
+        return run(() -> {
+            driveCurvature(leftAxis.getAsDouble(), rightAxis.getAsDouble());
+        });
+    }
+
+    public void drivePercent(double leftSpeed, double rightSpeed) {
+        leftLeaderMotor.setVoltage(leftSpeed * 12.0);
+        rightLeaderMotor.setVoltage(rightSpeed * 12.0);
+    }
+
+    public void driveArcade(double xSpeed, double zRotation) {
+        var speeds = DifferentialDrive.arcadeDriveIK(xSpeed, zRotation, false);
+
+        drivePercent(speeds.left, speeds.right);
+    }
+
+    public void driveCurvature(double xSpeed, double zRotation) {
+        var speeds = DifferentialDrive.curvatureDriveIK(xSpeed, zRotation, true);
+
+        drivePercent(speeds.left, speeds.right);
+    }
+
+    public void stop() {
+        leftLeaderMotor.stopMotor();
+        rightLeaderMotor.stopMotor();
+    }
+}
